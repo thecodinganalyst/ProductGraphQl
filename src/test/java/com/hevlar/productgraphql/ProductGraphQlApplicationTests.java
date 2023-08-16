dac237c43c4f23697b958b8190063310a601253d
Dennis Cai <dennis@thecodinganalyst.com>
2023-08-16 12:04:17 +0800

    add checks

--- src/test/java/com/hevlar/productgraphql/ProductGraphQlApplicationTests.java
+++ src/test/java/com/hevlar/productgraphql/ProductGraphQlApplicationTests.java
@@ -1,17 +1,25 @@
 package com.hevlar.productgraphql;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@Slf4j
 @ContextConfiguration(classes = MongoDBTestContainerConfig.class)
 class ProductGraphQlApplicationTests {
 
+    @Autowired
+    Environment environment;
+
     @Test
     void contextLoads() {
+        log.info(environment.getProperty("spring.data.mongodb.port"));
     }
 
 }
--- src/test/java/com/hevlar/productgraphql/controller/CategoryControllerTest.java
+++ src/test/java/com/hevlar/productgraphql/controller/CategoryControllerTest.java
@@ -3,22 +3,27 @@ package com.hevlar.productgraphql.controller;
 import com.hevlar.productgraphql.MongoDBTestContainerConfig;
 import com.hevlar.productgraphql.ProductGraphQlApplication;
 import com.hevlar.productgraphql.model.Category;
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = ProductGraphQlApplication.class)
-@AutoConfigureWebTestClient
 @AutoConfigureHttpGraphQlTester
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Testcontainers
+@Slf4j
 @ContextConfiguration(classes = MongoDBTestContainerConfig.class)
 public class CategoryControllerTest {
 
@@ -28,8 +33,13 @@ public class CategoryControllerTest {
     @Autowired
     HttpGraphQlTester httpGraphQlTester;
 
+    @Autowired
+    Environment environment;
+
     @Test
-    public void testGetCategories() {
+    @Order(1)
+    public void testAddTopCategory() {
+        log.info(environment.getProperty("spring.data.mongodb.port"));
         Category category = this.httpGraphQlTester.document(
                         """
                         mutation {
@@ -55,5 +65,56 @@ public class CategoryControllerTest {
                 .entity(Category.class)
                 .get();
         assertThat(category).hasFieldOrPropertyWithValue("name", "Furniture");
+        assertThat(category).hasFieldOrProperty("subCategories");
+        assertThat(category.subCategories()).hasSize(2);
+        List<String> subCategoryNames = category.subCategories().stream().map(Category::name).toList();
+        assertThat(subCategoryNames).hasSameElementsAs(List.of("Kitchen", "Living Room"));
+    }
+
+    @Test
+    @Order(2)
+    public void testAddCategoryToExisting() {
+        Category category = this.httpGraphQlTester.document(
+                        """
+                        mutation {
+                            addCategoryToExisting(
+                                newCategory: {
+                                    name: "Sofa"
+                                    subCategories: [
+                                        { name: "2-seater" },
+                                        { name: "3-seater" },
+                                        { name: "L-Shape" }
+                                    ]
+                                },
+                                existingCategory: ["Furniture", "Living Room"]
+                            ){
+                                name
+                                subCategories {
+                                    name
+                                }
+                            }
+                        }
+                        """
+                )
+                .execute()
+                .errors()
+                .verify()
+                .path("addCategoryToExisting")
+                .entity(Category.class)
+                .get();
+
+
+        assertThat(category).hasFieldOrPropertyWithValue("name", "Furniture");
+//        Category livingRoomCategory = category.subCategories()
+//                .stream()
+//                .filter(cat -> cat.name().equals("Living Room"))
+//                .findFirst()
+//                .get();
+//        Category sofaCategory = livingRoomCategory.subCategories().get(0);
+//        assertThat(sofaCategory).hasFieldOrPropertyWithValue("name", "Sofa");
+//        assertThat(sofaCategory.subCategories()).hasSize(3);
+//        List<String> sofaSubCategoryNames = sofaCategory.subCategories().stream().map(Category::name).toList();
+//        assertThat(sofaSubCategoryNames).hasSameElementsAs(List.of("2-seater", "3-seater", "L-Shape"));
+
     }
 }
